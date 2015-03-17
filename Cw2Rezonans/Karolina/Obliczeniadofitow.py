e7d5cf12d7f48708445ecc5deea4318a5d55dcc4
Dominik <stanczakdominik@gmail.com>
2015-03-17 19:50:46 +0100

    Zrobiona obliczeniówka. Wnioski

--- Cw2Rezonans/Karolina/Obliczeniadofitow.py
+++ Cw2Rezonans/Karolina/Obliczeniadofitow.py
@@ -13,7 +13,7 @@ df = 50 #(błąd odczytu częstotliwości do B)
 dB = sqrt(2)*df
 
 L=1/w0/w0/C
-dL=2*dw0/w0/w0/f0/C
+dL=2*dw0/w0/w0/w0/C
 print "L:", L, "+-", dL
 print "\n\n"
 
@@ -31,19 +31,25 @@ for R, Q, dQ in dane:
     print "Q_l:", Ql, "+-", dQl
     print "\n\n"
 
-Rg=50
-dane=[(1e4, 5.27268211252, 0.0282187840119), (3e4, 12.9617327208, 0.0665328450708)]
-for R, Q, dQ in dane:
-    print "R:", R
-    print "Q:",Q, "+-", dQ
-    #print 1/Q, 1/(w0*C*Rg)
-    Ql=1/(1/Q-1/(w0*C*Rg))
-    dQl1= dQ/(1-Q/C/Rg/w0)**2
-    dQl2=dw0*C*Q*Q*Rg/(Q-C*Rg*w0)**2
-    dQl=sqrt(dQl1**2+dQl2**2)
-    #drl=sqrt((L*dw0/Q)**2+(w0*dL/Q)**2+(w0*L*dQ/Q/Q)**2)
-    print "Ql:",Ql, "+-", dQl
-##    Ql=w0*L/rl
-##    dQl = sqrt((L*dw0/rl)**2+(w0*dL/rl)**2+(w0*L*drl/rl/rl)**2)
-##    print "Q_l:", Ql, "+-", dQl
-    print "\n\n"
+##Rg=50
+##dane=[(1e4, 5.27268211252, 0.0282187840119), (3e4, 12.9617327208, 0.0665328450708)]
+##for R, Q, dQ in dane:
+##    print "R:", R
+##    print "Q:",Q, "+-", dQ
+##    #print 1/Q, 1/(w0*C*Rg)
+##    Ql=1/(1/Q-1/(w0*C*Rg))
+##    dQl1= dQ/(1-Q/C/Rg/w0)**2
+##    dQl2=dw0*C*Q*Q*Rg/(Q-C*Rg*w0)**2
+##    dQl=sqrt(dQl1**2+dQl2**2)
+##    #drl=sqrt((L*dw0/Q)**2+(w0*dL/Q)**2+(w0*L*dQ/Q/Q)**2)
+##    print "Ql:",Ql, "+-", dQl
+####    Ql=w0*L/rl
+####    dQl = sqrt((L*dw0/rl)**2+(w0*dL/rl)**2+(w0*L*drl/rl/rl)**2)
+####    print "Q_l:", Ql, "+-", dQl
+##    print "\n\n"
+
+
+f0prime=10320
+C2ratio=f0*f0/f0prime/f0prime-1
+C2=C*C2ratio
+print "frequency ratio, C2:", f0prime/f0,C2ratio, C2
